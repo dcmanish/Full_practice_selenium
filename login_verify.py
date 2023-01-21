97f2636e071cc2a4907d5186f549306f8de32db6
Akki283 <akshaygaikwad283@gmail.com>
2023-01-21 14:26:41 +0530

    first

--- login_verify.py
+++ login_verify.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.service import Service
 driver=webdriver.Chrome()
 # driver=webdriver.Chrome(service=serv_obj)
 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+driver.find_element(By.NAME,"username").clear()
 driver.find_element(By.NAME,"username").send_keys("admin")
 driver.find_element(By.NAME,"password").send_keys("admin123")
 driver.find_element(By.XPATH,"//button[@type='submit']").click()
